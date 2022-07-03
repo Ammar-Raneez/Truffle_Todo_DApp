07efd086064e10b6ba7e5ce3788b19f1d191944f
Ammar <ammarraneez@gmail.com>
2022-07-03 11:42:36 +0530

    feat: create hardcoded task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-    uint public taskCount = 10;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("My first Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
